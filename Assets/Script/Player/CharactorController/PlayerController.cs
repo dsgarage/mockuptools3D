631bb89a4ff97dd73497e067760c38f66649ed30
Daisuke Tsukada <dsgarage@gmail.com>
2019-07-05 13:53:01 +0900

    CharactorController版PlayerController実装

--- Assets/Script/Player/CharactorController/PlayerController.cs
+++ Assets/Script/Player/CharactorController/PlayerController.cs
@@ -1,18 +1,92 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Script.Player.CharactorController
 {
-    // Start is called before the first frame update
-    void Start()
+    enum CaractorControllerType
     {
-        
+        Move,
+        SimpleMove,
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    [RequireComponent(typeof(CharacterController))]
+    public class PlayerController : MonoBehaviour
     {
+        [SerializeField]private CaractorControllerType _caractorControllerType = CaractorControllerType.Move;
         
+        [SerializeField]private float speed = 3.0F;
+        [SerializeField]private float rotateSpeed = 3.0F;
+        public float jumpPower = 5;
+        private Vector3 moveDirection;
+        public float gravity = 10f;
+        
+        
+        [SerializeField]private CharacterController controller;
+
+        void Start()
+        {
+            // コンポーネントの取得
+            controller = GetComponent<CharacterController>();
+
+        }
+
+        void Update()
+        {
+            switch (_caractorControllerType)
+            {
+                case CaractorControllerType.Move:
+                    CharactorcontrollerMove();
+                    break;
+                case CaractorControllerType.SimpleMove:
+                    CharactorcontrollerSimpleMove();
+                    break;
+
+                default:
+                    CharactorcontrollerMove();
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+        }
+
+        void CharactorcontrollerMove()
+        {
+            if (controller.isGrounded)
+            {
+                // 回転
+                transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+                // キャラクターのローカル空間での方向
+                moveDirection = transform.transform.forward * speed * Input.GetAxis("Vertical");
+
+                // ジャンプ
+                if (Input.GetButtonDown("Jump")) moveDirection.y = jumpPower;
+
+            }
+            else
+            {
+
+                // 重力
+                moveDirection.y -= gravity * Time.deltaTime;
+
+            }
+
+            // Move関数で移動させる
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+
+        void CharactorcontrollerSimpleMove()
+        {
+            // 回転
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+            // キャラクターのローカル空間での方向
+            Vector3 forward = transform.transform.forward;
+
+            float curSpeed = speed * Input.GetAxis("Vertical");
+
+            // SimpleMove関数で移動させる
+            controller.SimpleMove(forward * curSpeed);
+        }
+
     }
 }
