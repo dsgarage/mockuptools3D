ae9a72abc859e1ef89720f6b8c423937867895ce
Daisuke Tsukada <dsgarage@gmail.com>
2019-07-05 19:28:00 +0900

    カメラのスイッチャ カメラ越しに弾を撃つコンポーネント

---
+++ Assets/Script/BuletComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BuletComponent : MonoBehaviour
+{
+	[SerializeField] private float power = 50f;
+	[SerializeField] private float deleteTime = 10f;
+	private Rigidbody rigid;
+	private Ray ray;
+ 
+	void Awake() {
+		//　Rigidbodyを取得し速度を0に初期化
+		rigid = GetComponent <Rigidbody> ();
+	}
+ 
+	//　弾がアクティブになった時
+	void OnEnable() {
+ 
+		//　カメラからクリックした位置にレイを飛ばす
+		ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+ 
+		//　弾を発射してから指定した時間が経過したら自動で削除
+		Destroy (this.gameObject, deleteTime);
+	}
+ 
+	void OnCollisionEnter(Collision col) {
+		// Enemyタグがついた敵に衝突したら自身と敵を削除
+		if (col.gameObject.CompareTag ("Enemy")) {
+			Destroy (this.gameObject);
+			Destroy (col.gameObject);
+		}
+	}
+	//　弾が存在していればレイの方向に力を加える
+	void FixedUpdate() {
+		rigid.AddForce (ray.direction * power, ForceMode.Force);
+	}
+}
---
+++ Assets/Script/ObjectShooter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShooter : MonoBehaviour
+{
+	//　弾のプレハブ
+	[SerializeField] private GameObject bullet;
+	//　レンズからのオフセット値
+	[SerializeField] private float offset;
+
+	[SerializeField] private float waitTime = 0.1f;
+	
+	//　経過時間
+	private float elapsedTime = 0f;
+	
+	private void Start()
+	{
+		if (!bullet)
+		{
+			SetBullet();
+		}
+	}
+
+	void Update () {
+		
+		elapsedTime += Time.deltaTime;
+		if (elapsedTime < waitTime) {
+			return;
+		}
+
+		if (Input.GetButton ("Fire1")) {
+			//　カメラのレンズの中心を求める
+			var centerOfLens = UnityEngine.Camera.main.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, UnityEngine.Camera.main.nearClipPlane + offset));
+			//　カメラのレンズの中心から弾を飛ばす
+			var bulletObj = Instantiate (bullet, centerOfLens, Quaternion.identity) as GameObject;
+		}
+		
+		if (bullet == null)
+		{
+			return;
+		}
+
+	}
+
+	private void SetBullet()
+	{
+		SetBullet(GameObject.CreatePrimitive(PrimitiveType.Sphere));
+	}
+	
+	private void SetBullet(GameObject bulletObject)
+	{
+		bullet = bulletObject;
+		try
+		{
+			bullet.GetComponent<BuletComponent>();
+			bullet.GetComponent<Rigidbody>().isKinematic = true;
+
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e);
+			bullet.AddComponent<BuletComponent>();
+			bullet.GetComponent<Rigidbody>().isKinematic = false;
+			throw;
+		}
+	}
+}
